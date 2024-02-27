ae4e27e4882535de6bc255ec2246f5d9969e223d
FarhanVirenze <farhanvirenze18@gmail.com>
2024-02-27 18:08:41 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+document.getElementById("watchButton").addEventListener("click", function() {
+    window.location.href = "https://185.215.164.57/avengers-endgame-2019/";
+});
+
+document.getElementById("watchButton1").addEventListener("click", function() {
+    window.location.href = "https://185.215.164.57/insidious-the-red-door-2023/";
+});
+
+document.getElementById("watchButton2").addEventListener("click", function() {
+    window.location.href = "https://5.253.86.213/fast-x-2023/";
+});
+
+document.getElementById("watchButton3").addEventListener("click", function() {
+    window.location.href = "http://103.187.147.50/society-of-the-snow-2023/";
+});
+
+document.getElementById("watchButton4").addEventListener("click", function() {
+    window.location.href = "https://amp.lk21official.mom/godzilla-king-of-the-monsters-2019/";
+});
+
+document.getElementById("watchButton5").addEventListener("click", function() {
+    window.location.href = "https://165.22.96.201/interstellar/";
+});
+
+document.getElementById("watchButton6").addEventListener("click", function() {
+    window.location.href = "https://185.215.164.57/the-conjuring-2-2016/";
+});
+
+document.getElementById("watchButton7").addEventListener("click", function() {
+    window.location.href = "https://185.215.164.57/badland-hunters-2024/";
+});
+
+document.getElementById("watchButton8").addEventListener("click", function() {
+    window.location.href = "https://194.233.90.34/divergent-2014/";
+});
+
+document.getElementById("watchButton9").addEventListener("click", function() {
+    window.location.href = "https://filmapik.moe/nonton-film-titanic-1997-subtitle-indonesia/play";
+});
+
+document.getElementById("watchButton10").addEventListener("click", function() {
+    window.location.href = "https://101.99.91.147/invisible-guest-2017/";
+});
+
+document.getElementById("watchButton11").addEventListener("click", function() {
+    window.location.href = "https://194.233.77.161/movie/la-la-land-2016-he4tmoa/";
+});
